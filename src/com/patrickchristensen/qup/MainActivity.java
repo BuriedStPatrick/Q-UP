9387833e05970ce9ae5ecbe81dffc0201c77af59
Patrick Christensen <patrick.christensen@eksponent.com>
2014-11-28 13:39:50 +0100

    Added some toasting goodness

--- src/com/patrickchristensen/qup/MainActivity.java
+++ src/com/patrickchristensen/qup/MainActivity.java
@@ -1,48 +1,47 @@
 package com.patrickchristensen.qup;
 
-import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.support.v7.internal.widget.AdapterViewCompat.OnItemClickListener;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnItemClickListener{
 	
 	public static final String SERVERIP = "";
 	public static final int SERVERPORT = 8080;
+	private String actionBar = "Now Playing";
 	
 	private ActionBarDrawerToggle 	drawerListener;
+	private ListView				drawerList;
 	private DrawerLayout			drawerLayout;
 	private Toolbar					toolbar;
 	
 	private TextView serverStatus;
-	private LinearLayout mainLayout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
-		
-		if(toolbar != null){
-			toolbar.setTitle("Navigation Drawer");
-			setSupportActionBar(toolbar);
-		}
-		
 		initDrawer();
 	}
 	
 	private void initView(){
 		toolbar = (Toolbar) findViewById(R.id.toolbar);
-		
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		serverStatus = (TextView) findViewById(R.id.server_status);
+		drawerList = (ListView) findViewById(R.id.drawer_list);
+		updateViews();
 	}
 	
 	private void initDrawer(){
@@ -50,14 +49,37 @@ public class MainActivity extends ActionBarActivity {
 			@Override
 			public void onDrawerOpened(View drawerView) {
 				super.onDrawerOpened(drawerView);
+				invalidateOptionsMenu();
 			}
 			
 			@Override
 			public void onDrawerClosed(View drawerView) {
 				super.onDrawerClosed(drawerView);
+				invalidateOptionsMenu();
 			}
 		};
-		drawerLayout.setDrawerListener(drawerListener);	
+		drawerLayout.setDrawerListener(drawerListener);
+		drawerList.setOnItemClickListener((android.widget.AdapterView.OnItemClickListener) this);
+		
+		drawerLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				drawerListener.syncState();
+			}
+		});
+	}
+	
+	private void updateViews(){
+		if(toolbar != null){
+			toolbar.setTitle(actionBar);
+			setSupportActionBar(toolbar);
+		}
+	}
+	
+	@Override
+	public void onItemClick(AdapterViewCompat<?> parent, View view, int position,
+			long id) {
+		Toast.makeText(this, position, Toast.LENGTH_LONG).show();
 	}
 	
 	@Override
