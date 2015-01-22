134af4db4403b624d44301f149c7e26b06acd2bc
Patrick Christensen <patrick.christensen@eksponent.com>
2015-01-22 09:41:15 +0100

    Added a bunch of comments

--- src/com/patrickchristensen/qup/threads/ReceiverThread.java
+++ src/com/patrickchristensen/qup/threads/ReceiverThread.java
@@ -17,24 +17,30 @@ import com.patrickchristensen.qup.util.Utils;
 public class ReceiverThread implements Runnable{
 	
 	
-	public static String serverIp = "";
+	public static String localIp = "";
 	
 	private Handler				handler;
 	private ServerSocket		receiverSocket;
 	
+	/**
+	 * The constructor of this runnable
+	 * @param handler The handler that communicates back to the Activity
+	 */
 	public ReceiverThread(Handler handler){
 		this.handler = handler;
-		serverIp = Utils.getIPAddress(true);
+		localIp = Utils.getIPAddress(true); //get and set the local IP address
 	}
 
 	@Override
 	public void run() {
 		try {
-            if (serverIp != null) {
+            if (localIp != null) {
                 receiverSocket = new ServerSocket(QupApplication.serverPort);
+                //Infinite loop, since we want to keep listening for as long as the application runs
                 while (true) {
-                    // LISTEN FOR INCOMING CLIENTS
-                	Socket sender = receiverSocket.accept(); // Blocking call
+                    //Listen for incoming messages
+                	Socket sender = receiverSocket.accept(); // Blocking call, will "pause" the code until a connection occurs
+                	//When a connection is found, start an action accordingly in a separate thread
                 	new Thread(new ActionThread(sender)).start();
                 }     
             }
@@ -53,14 +59,23 @@ public class ReceiverThread implements Runnable{
 		
 		@Override
 		public void run() {
+			//To catch any exceptions (things that go wrong), we have to tell it to try and catch all errors, so the app doesn't crash
 			try {
+				//Get the stream of data (the data JSON string) from the sender
                 BufferedReader in = new BufferedReader(new InputStreamReader(sender.getInputStream()));
                 String line = null;
+                //Go over each line and execute a command accordingly. 1 line = 1 command
                 while ((line = in.readLine()) != null) {
+                	//Obtain message from pool. This means that we only allocate memory for a new Message object if we can't already use an existing
+                	//Messages are used to communicate data with Handlers
                 	Message msg = Message.obtain();
+                	//Bundle is required for putting data in a Message
                 	Bundle bundle = new Bundle();
+                	//We put the JSON string in the Bundle with a key "data"
                 	bundle.putString("data", line);
+                	//We set the Message's data to the instance of the Bundle
                 	msg.setData(bundle);
+                	//We send the message with the data to the Handler that then executes on the Activity
                 	handler.sendMessage(msg);
                 }
             }catch(IOException ioe){
@@ -72,7 +87,9 @@ public class ReceiverThread implements Runnable{
 	}
 	
 	public void stop(){
+		//If the application stops, so we need to stop the thread as well
 		try {
+			//Closes our socket
 			receiverSocket.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/com/patrickchristensen/qup/threads/SenderThread.java
+++ src/com/patrickchristensen/qup/threads/SenderThread.java
@@ -19,36 +19,48 @@ import com.patrickchristensen.qup.QupApplication;
 import com.patrickchristensen.qup.commands.Command;
 
 //TODO: Turn into AsyncTask
-public class SenderThread implements Runnable{
-	
+public class SenderThread implements Runnable {
+
 	private String receiverIpAddress;
 	private Command command;
-	
+
+	/**
+	 * The constructor of this runnable
+	 * 
+	 * @param command
+	 *            The command we want to execute
+	 */
 	public SenderThread(Command command) {
+		// Get the receiverIp from the command
 		this.receiverIpAddress = command.getReceiverIp();
 		this.command = command;
 	}
 
 	@Override
 	public void run() {
+		// Try this code, catch any errors so the app doesn't crash
 		try {
-            InetAddress serverAddr = InetAddress.getByName(receiverIpAddress);
-            Socket socket = new Socket(serverAddr, QupApplication.serverPort);
-                try {
-                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
-                                .getOutputStream())), true);
-                    Looper.prepare();
-                	Gson json = new Gson();
-                	out.println(json.toJson(command));
-                } catch (Exception e) {
-                    Log.e("ClientActivity", "S: Error", e);
-                }
-                
-            socket.close();
-        } catch (Exception e) {
-            Log.e("ClientActivity", "C: Error", e);
-            ClientActivity.connected = false;
-        }
+			// Instantiate a InetAddress object required for socket connections
+			InetAddress serverAddr = InetAddress.getByName(receiverIpAddress);
+			Socket socket = new Socket(serverAddr, QupApplication.serverPort);
+			try {
+				// Write an output stream to the socket (our JSON data)
+				PrintWriter out = new PrintWriter(new BufferedWriter(
+						new OutputStreamWriter(socket.getOutputStream())), true);
+				Looper.prepare();
+				// Use the GSON library to write our command to a JSON string
+				Gson json = new Gson();
+				// Write to the socket's output stream
+				out.println(json.toJson(command));
+			} catch (Exception e) {
+				Log.e("ClientActivity", "S: Error", e);
+			}
+			// Close the socket once the action is complete
+			socket.close();
+		} catch (Exception e) {
+			Log.e("ClientActivity", "C: Error", e);
+			ClientActivity.connected = false;
+		}
 	}
 
 }
